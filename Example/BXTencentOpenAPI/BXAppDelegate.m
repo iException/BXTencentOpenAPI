3551a921c47c3fe2082af21342fcd5f0d5c0a604
Yiming Tang <yimingnju@gmail.com>
2016-01-29 16:25:17 +0800

    Add OAuth demo

--- Example/BXTencentOpenAPI/BXAppDelegate.m
+++ Example/BXTencentOpenAPI/BXAppDelegate.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2016 Yiming Tang. All rights reserved.
 //
 
+#import <TencentOpenAPI/TencentOAuth.h>
 #import "BXAppDelegate.h"
 #import "BXViewController.h"
 
@@ -13,12 +14,30 @@
 
 @synthesize window = _window;
 
+
+#pragma mark - UIApplicationDelegate
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
     self.window.rootViewController = [[BXViewController alloc] init];
     [self.window makeKeyAndVisible];
     return YES;
+}
+
+
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
+    return [TencentOAuth HandleOpenURL:url];
+}
+
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    return [TencentOAuth HandleOpenURL:url];
+}
+
+
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options {
+    return [TencentOAuth HandleOpenURL:url];
 }
 
 @end
--- Example/BXTencentOpenAPI/BXViewController.m
+++ Example/BXTencentOpenAPI/BXViewController.m
@@ -6,15 +6,22 @@
 //  Copyright (c) 2016 Yiming Tang. All rights reserved.
 //
 
-#import <TencentOpenAPI/QQApiInterface.h>
+#import <TencentOpenAPI/TencentOAuth.h>
 #import "BXViewController.h"
 
 
-@interface BXViewController ()
+#warning You should replace `YOUR_APP_ID` with your own app ID provided by Tencent.
+static NSString *const kBXAppID = @"YOUR_APP_ID";
+
+
+@interface BXViewController () <TencentSessionDelegate>
 
 @property (nonatomic) UILabel *textLabel;
+@property (nonatomic) UIButton *submitButton;
+@property (nonatomic) TencentOAuth *oauth;
 
 @end
+
 
 @implementation BXViewController
 
@@ -26,8 +33,21 @@
         _textLabel.translatesAutoresizingMaskIntoConstraints = NO;
         _textLabel.font = [UIFont systemFontOfSize:18.0f];
         _textLabel.textAlignment = NSTextAlignmentCenter;
+        _textLabel.text = @"Tencent OAuth";
     }
     return _textLabel;
+}
+
+
+- (UIButton *)submitButton {
+    if (!_submitButton) {
+        _submitButton = [[UIButton alloc] init];
+        _submitButton.translatesAutoresizingMaskIntoConstraints = NO;
+        [_submitButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+        [_submitButton setTitle:@"Sign In" forState:UIControlStateNormal];
+        [_submitButton addTarget:self action:@selector(signIn:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _submitButton;
 }
 
 
@@ -37,18 +57,51 @@
     [super viewDidLoad];
     
     [self.view addSubview:self.textLabel];
+    [self.view addSubview:self.submitButton];
     [self setupViewConstraints];
-    
-    self.textLabel.text = [QQApiInterface getQQInstallUrl];
+}
+
+
+#pragma mark - Actions
+
+- (void)signIn:(id)sender {
+    self.oauth = [[TencentOAuth alloc] initWithAppId:kBXAppID andDelegate:self];
+    NSArray *permissions = @[kOPEN_PERMISSION_GET_USER_INFO,
+                             kOPEN_PERMISSION_GET_SIMPLE_USER_INFO,
+                             kOPEN_PERMISSION_GET_INFO,
+                             kOPEN_PERMISSION_ADD_SHARE];
+    [self.oauth authorize:permissions];
 }
 
 
 #pragma mark - Private
 
 - (void)setupViewConstraints {
-    UILabel *textLabel = self.textLabel;
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-50.0-[textLabel]" options:kNilOptions metrics:nil views:NSDictionaryOfVariableBindings(textLabel)]];
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[textLabel]-|" options:kNilOptions metrics:nil views:NSDictionaryOfVariableBindings(textLabel)]];
+    NSDictionary *views = @{
+        @"textLabel": self.textLabel,
+        @"submitButton": self.submitButton,
+    };
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-100.0-[textLabel]-50.0-[submitButton]" options:kNilOptions metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[textLabel]-|" options:kNilOptions metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[submitButton]-|" options:kNilOptions metrics:nil views:views]];
+}
+
+
+#pragma mark - TencentSessionDelegate
+
+- (void)tencentDidLogin {
+    self.textLabel.text = [NSString stringWithFormat:@"Access Token: %@", self.oauth.accessToken];
+}
+
+
+- (void)tencentDidNotLogin:(BOOL)cancelled {
+    self.textLabel.text = @"Failed";
+}
+
+
+- (void)tencentDidNotNetWork {
+    self.textLabel.text = @"Networking Error";
 }
 
 @end
